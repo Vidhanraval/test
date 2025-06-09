5aa85057ec1d077b361a3c989269b3faea54e471
Vidhan Raval <214603023+Vidhanraval@users.noreply.github.com>
2025-06-09 11:45:00 +0530

    changes

---
+++ src/app/xyz/page.tsx
@@ -0,0 +1,13 @@
+import Form from '@/components/form'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      hello xyz 1323
+      <Form/>
+    </div>
+  )
+}
+
+export default page
